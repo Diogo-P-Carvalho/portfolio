368f08470b45491f4a4f11840ea2f1d070900a70
Diogo-P-Carvalho <dpcarvalho86@gmail.com>
2021-05-23 13:02:31 +0100

    html struture

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,23 @@
+import aboutImg from '../../assets/about.jpg';
+
 function About() {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <section>
+      <h2>01 : About</h2>
+
+      <article className="about-article">
+
+        <div id="about">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum maiores corporis consectetur hic quasi soluta temporibus eveniet culpa eos velit sed laboriosam minus quod numquam exercitationem laborum voluptatum, ut mollitia.</p>
+          <h3 className="highlights">I'm driven by the love of turning ideas into reality!</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur repudiandae fugit veniam a dolores laborum eius quam, consequuntur porro nihil cumque nam nulla eaque atque ullam doloribus adipisci eveniet.</p>
+          <h3 className="highlights">Good user experience is finding that perfect balance between the way something looks and how it works.</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil maxime dicta, impedit nesciunt saepe possimus qui exercitationem fugit, dolorem illum magnam reiciendis incidunt itaque porro tempora officia debitis aspernatur dolorum?</p>                
+        </div>  
+
+        <img src={aboutImg} alt="It's me!"/> 
+      </article>
+    </section>
   );
 }
 
