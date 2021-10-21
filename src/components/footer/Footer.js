0a792ba779a416cf5f1a4d0c5a4b2279cb04e204
Diogo-P-Carvalho <dpcarvalho86@gmail.com>
2021-10-21 23:19:17 +0100

    changed texts and some styles

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 function Footer() {
     return (
         <footer>
-            Diogo Carvalho &copy; <span>{new Date().getFullYear()}</span> | <span className="footer-message">Made with love and lots of coffee!</span>
+            Diogo Carvalho &copy; <span>{new Date().getFullYear()}</span> | <span className="footer-message">Made with love and lots of coffee &#9749;!</span>
         </footer>
     )
 }
