a37e2f4c25ca03b6eb3e9f0d82372b8e70c078a8
Diogo-P-Carvalho <dpcarvalho86@gmail.com>
2021-10-31 10:52:05 +0000

    Updates

--- src/components/logo/Logo.js
+++
@@ -1,7 +0,0 @@
-import logoImg from '../../assets/logo.svg';
-
-export default function Logo({ className }) {
-    return (
-        <img src={logoImg} alt="logo" className={className}></img>
-    )
-}
